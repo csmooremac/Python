ba2df8282912eaa95ac76e5bc4542051a6daa309
csmooremac <csmooremac@yahoo.com>
2015-09-19 17:51:00 -0600

    Ask a Question-Add Statement
    Assignment was to ask the user a question then put the answer in a
    variable along with a statement.  I also added a description about the
    the small program

---
+++ WhatCollegeAttending.py
@@ -0,0 +1,12 @@
+# This small program will ask the user what college they are or plan on attending
+# A description statement will explain why the user is attending the college 
+# The 2 answers are put together to print out a complete sentance. 
+
+college = input('What college are you attending, or plan on attending?')
+description = input("Why are you attending" + college + "?")
+collegeStatement = ('I am attending' + college + description)
+print(collegeStatement)
+
+
+
+
